aa5063361850a249452dfc589f18319d79108f53
Stefan Arambasich <ArtisOracle@gmail.com>
2018-02-26 00:53:34 -0500

    Restructure the project and its files and folders

---
+++ Source/SACircularCountdown/SACircularCountdown.h
@@ -0,0 +1,19 @@
+//
+//  SACircularCountdown.h
+//  SACircularCountdown
+//
+//  Created by Stefan Arambasich on 2/26/18.
+//  Copyright © 2018 Stefan Arambasich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SACircularCountdown.
+FOUNDATION_EXPORT double SACircularCountdownVersionNumber;
+
+//! Project version string for SACircularCountdown.
+FOUNDATION_EXPORT const unsigned char SACircularCountdownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SACircularCountdown/PublicHeader.h>
+
+
